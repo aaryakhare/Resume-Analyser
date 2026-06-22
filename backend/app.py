819692c37abd894ac5b04fb8f5dbbfb166ee37ea
Aarya Khare <aaryakhare.12@gmail.com>
2026-06-22 15:33:45 +0530

    Skill Detection added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,26 @@
 import pdfplumber
 from flask import Flask, request, jsonify
 import os
-
+SKILLS = [
+    "python",
+    "java",
+    "c++",
+    "html",
+    "css",
+    "javascript",
+    "react",
+    "nodejs",
+    "mongodb",
+    "mysql",
+    "sql",
+    "git",
+    "github",
+    "flask",
+    "machine learning",
+    "deep learning",
+    "data structures",
+    "algorithms"
+]
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
@@ -24,6 +43,19 @@ def extract_text_from_pdf(pdf_path):
                 text += page_text + "\n"
 
     return text
+def detect_skills(text):
+
+    found_skills = []
+
+    text = text.lower()
+
+    for skill in SKILLS:
+
+        if skill.lower() in text:
+
+            found_skills.append(skill)
+
+    return found_skills
 
 @app.route("/")
 def home():
@@ -53,10 +85,12 @@ def upload_resume():
     file.save(filepath)
 
     resume_text = extract_text_from_pdf(filepath)
-
+    detected_skills = detect_skills(resume_text)
+    
     return jsonify({
     "message": "Resume uploaded successfully",
     "filename": file.filename,
+    "skills": detected_skills,
     "text": resume_text
     })
 
