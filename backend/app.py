594616810359b1276ab5e23b6e64cd6b70390240
Aarya Khare <aaryakhare.12@gmail.com>
2026-06-22 15:13:19 +0530

    Added PDF Upload feature

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,47 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
 @app.route("/")
 def home():
-    return "AI Resume Analyzer Backend Running!"
+    return "Backend Running"
+
+
+@app.route("/upload", methods=["POST"])
+def upload_resume():
+
+    if "resume" not in request.files:
+        return jsonify({
+            "error": "No file uploaded"
+        }), 400
+
+    file = request.files["resume"]
+
+    if file.filename == "":
+        return jsonify({
+            "error": "No file selected"
+        }), 400
+
+    filepath = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        file.filename
+    )
+
+    file.save(filepath)
+
+    return jsonify({
+        "message": "Resume uploaded successfully",
+        "filename": file.filename
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
